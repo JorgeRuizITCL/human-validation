c70cf0c1b95db8d2ec2e033b1c85570fad716574
Jorge Ruiz <jorge.ruiz@itcl.es>
2023-10-09 12:09:19 +0200

    rename dockerfile & extend validator config

--- human_val/validator.py
+++ human_val/validator.py
@@ -16,20 +16,20 @@ DEFAULT_PROVIDERS =  [("CUDAExecutionProvider", {"cudnn_conv_use_max_workspace":
 
 class Validator:
     
-    def __init__(self, model_path: str, onnx_providers: Any = None, min_conf_thres: float = 0.66) -> None:
+    def __init__(self, model_path: str, onnx_providers: Any = None, min_conf_thres: float = 0.5) -> None:
         onnx_providers = DEFAULT_PROVIDERS if onnx_providers is None else onnx_providers
         self._conf_thres = min_conf_thres
         self._sess = ort.InferenceSession(model_path, providers=onnx_providers)
         input_node = self._sess.get_inputs()[0]
         self._input_name, self._input_shape = input_node.name, input_node.shape
     
-    def from_movenet_lightning():
+    def from_movenet_lightning(onnx_providers: Any = None, min_conf_thres: float = 0.5):
         path = _LIB_PTH / "models/onnx/lite-model_movenet_singlepose_lightning_3_op15.onnx"
-        return Validator(str(path))
+        return Validator(str(path), onnx_providers=onnx_providers, min_conf_thres=min_conf_thres)
     
-    def from_movenet_thunder():
+    def from_movenet_thunder(onnx_providers: Any = None, min_conf_thres: float = 0.5):
         path = _LIB_PTH / "models/onnx/lite-model_movenet_singlepose_thunder_3_op15.onnx"
-        return Validator(str(path))
+        return Validator(str(path), onnx_providers=onnx_providers, min_conf_thres=min_conf_thres)
         
     def validate(self, img_rgb_u8: np.ndarray, min_joints: int) -> Tuple[List[Joint], bool]:
         reshaped_img = resize_pad_crop(img_rgb_u8, self._input_shape[-3:-1])
